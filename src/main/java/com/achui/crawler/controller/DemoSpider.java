d55e6553c19d8f2b5911cdac76c509b344560c3b
achui_1980 <achui1980@gmail.com>
2020-07-26 22:34:21 +0800

    基本步骤

--- src/main/java/com/achui/crawler/controller/DemoSpider.java
+++ src/main/java/com/achui/crawler/controller/DemoSpider.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,37 +24,49 @@ public class DemoSpider {
 
     @GetMapping("/spider")
     private String spider() throws Exception {
-        String url = "https://molina.vuesoftware.com/PORTAL.STS/Pages/MolinaPP/Login.aspx?ReturnUrl=%2fPORTAL.STS%2fdefault.aspx%3fwa%3dwsignin1.0%26wtrealm%3dhttps%253a%252f%252fmolina.vuesoftware.com%252fPORTAL%252f%26wct%3d2018-09-20T22%253a43%253a34Z%26wcrt%3dMjAvU2VwLzIwMTggMjI6NDM6MzQgUE0%25253d&wa=wsignin1.0&wtrealm=https%3a%2f%2fmolina.vuesoftware.com%2fPORTAL%2f&wct=2018-09-20T22%3a43%3a34Z&wcrt=MjAvU2VwLzIwMTggMjI6NDM6MzQgUE0%253d";
-        String userName = "AA0075";
+        String url = "https://panjiachen.github.io/vue-element-admin/#/login";
+        String userName = "admin";
         String password = "D*u4HlX4P8ey";
-        String userInput = "//*[@id=\"txtUserName\"]";
-        String passwordInput = "//*[@id=\"txtPassword\"]";
-        String loginBtn = "//*[@id=\"btnLogin\"]";
-
-        //*[@id="egSmartVUEGrid"]/div[4]/ul/li[7]/a
+        String userInput = "//*[@id=\"app\"]/div/form/div[2]/div/div/input";
+        String passwordInput = "//*[@id=\"app\"]/div/form/div[3]/div/div/input";
+        String loginBtn = "//*[@id=\"app\"]/div/form/button";
         WebDriver webDriver = pool.getWebDriverFromPool();
+        /******************** Login *************************/
         webDriver.get(url);
         WebElement usernameEle = ((ChromeDriver) webDriver).findElementByXPath(userInput);
         WebElement passwordEle = ((ChromeDriver) webDriver).findElementByXPath(passwordInput);
         WebElement loginEle = ((ChromeDriver) webDriver).findElementByXPath(loginBtn);
-        usernameEle.sendKeys(userName);
-        passwordEle.sendKeys(password);
+        //usernameEle.sendKeys(userName);
+        //passwordEle.sendKeys(password);
         loginEle.click();
-//        Thread.sleep(3000);
-//        webDriver.get("https://molina.vuesoftware.com/portal/Common/GlobalSearch");
-        WebElement menu = pool.getWebDriverFactory().getWait().until(d -> d.findElement(By.xpath("//*[@id=\"leftPanel\"]/li[5]")));
-        Actions action = new Actions(webDriver);
-        action.moveToElement(menu).perform();
-        WebElement btn = ((ChromeDriver) webDriver).findElementByXPath("//*[@id=\"leftPanel\"]/li[5]/ul/li[2]");
-        //action.moveToElement(btn).click().perform();
-        //action.click(btn);
-        btn.click();
-        System.out.println(btn.getAttribute("data-uid"));
-        WebElement searchBtl = pool.getWebDriverFactory().getWait().until(d -> d.findElement(By.xpath("//*[@id=\"SmartVUEPanel_AdvSearch_btnSearch\"]")));
-        WebElement agentName = ((ChromeDriver) webDriver).findElementByXPath("//*[@id=\"SmartVUEPanel_AdvSearch_4_2_0\"]")
-        agentName.sendKeys("");
-        System.out.println(searchBtl.getText());
+        /******************** Login *************************/
 
-        return webDriver.getTitle();
+        /******************** Got To Search Page *************************/
+        WebElement tableMenu = pool.getWebDriverFactory().getWait().until(d -> d.findElement(By.xpath("//*[@id=\"app\"]/div/div[1]/div/div[1]/div/ul/div[9]/li")));
+        tableMenu.click();
+        WebElement table = pool.getWebDriverFactory().getWait().until(d -> d.findElement(By.xpath("//*[@id=\"app\"]/div/div[1]/div/div[1]/div/ul/div[9]/li/ul/div[4]/a/li")));
+        table.click();
+        /******************** Got To Search Page *************************/
+
+        /******************** Search Result *************************/
+        WebElement searchBtn = pool.getWebDriverFactory().getWait().until(d -> d.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/section/div/div[1]/button[1]")));
+        WebElement searchInput = pool.getWebDriverFactory().getWait().until(d -> d.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/section/div/div[1]/div[1]/input")));
+        searchInput.sendKeys("F");
+        searchBtn.click();
+
+        WebElement mask = webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/section/div/div[2]/div[5]"));
+        pool.getWebDriverFactory().getWait().until(ExpectedConditions.not(ExpectedConditions.visibilityOf(mask)));
+        /******************** Search Result *************************/
+        //TODO: get data from first page
+        WebElement nextPage = webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/section/div/div[3]/div/button[2]"));
+        while (nextPage.getAttribute("disabled") == null) {
+            nextPage.click();
+            mask = webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/section/div/div[2]/div[5]"));
+            pool.getWebDriverFactory().getWait().until(ExpectedConditions.not(ExpectedConditions.visibilityOf(mask)));
+            //TODO get data from second page
+        }
+        /******************** Search Result *************************/
+
+        return searchBtn.getText();
     }
 }
